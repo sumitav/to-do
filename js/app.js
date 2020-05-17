d960d2c244dd61da3fb4887ae77e2de5c8becdd0
Sumitav Acharya <sumitavacharya11@gmail.com>
2020-05-17 21:27:22 +0530

    hello

---
+++ js/app.js
@@ -0,0 +1,116 @@
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+let LIST, id;
+
+/*------------------------------------------------------get item from localstorage---------------------------------------------*/
+let data = localStorage.getItem("TODO");
+if(data){
+    LIST = JSON.parse(data);
+    id = LIST.length;
+    loadList(LIST);
+}
+else{
+    LIST = [];
+    id = 0;
+}
+
+/*------------------------------------------------------load items to interface------------------------------------------------------*/
+function loadList(array){
+    array.forEach(function(item){
+        addToDo(item.name, item.id, item.done, item.trash);
+    });
+}
+/*------------------------------------------------------clear storage--------------------------------------------------*/
+clear.addEventListener("click", function(){
+    localStorage.clear();
+    location.reload();
+});
+
+/*----------------------------------------------------for date------------------------------------------------------*/
+const options = {weekday : "long", month:"short", day:"numeric"};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+/*------------------------------------------------------add to do function---------------------------------------------*/
+
+function addToDo(toDo, id, done, trash){
+    
+    if(trash){ return; }
+    
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_THROUGH : "";
+    
+    const item = `<li class="item">
+                    <i class="fa ${DONE} co" job="complete" id="${id}"></i>
+                    <p class="text ${LINE}">${toDo}</p>
+                    <i class="fa fa-trash-o de" job="delete" id="${id}"></i>
+                  </li>
+                `;
+    
+    const position = "beforeend";
+    
+    list.insertAdjacentHTML(position, item);
+}
+
+/*------------------------------------------------------Add an item to the list user the enter key-------------------------------*/
+document.addEventListener("keyup",function(even){
+    if(event.keyCode == 13){
+        const toDo = input.value;
+        
+        // if the input isn't empty
+        if(toDo){
+            addToDo(toDo, id, false, false);
+            
+            LIST.push({
+                name : toDo,
+                id : id,
+                done : false,
+                trash : false
+            });
+            localStorage.setItem("TODO", JSON.stringify(LIST));
+            
+            id++;
+        }
+        input.value = "";
+    }
+});
+
+
+/*------------------------------------------------------complete to do=---------------------------------------------------------*/
+function completeToDo(element){
+    element.classList.toggle(CHECK);
+    element.classList.toggle(UNCHECK);
+    element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+    
+    LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+/*------------------------------------------------------remove to do----------------------------------------------------------*/
+function removeToDo(element){
+    element.parentNode.parentNode.removeChild(element.parentNode);
+    
+    LIST[element.id].trash = true;
+}
+
+/*------------------------------------------------------target the items created dynamically-----------------------------------------------*/
+
+list.addEventListener("click", function(event){
+    const element = event.target; // return the clicked element inside list
+    const elementJob = element.attributes.job.value; // complete or delete
+    
+    if(elementJob == "complete"){
+        completeToDo(element);
+    }else if(elementJob == "delete"){
+        removeToDo(element);
+    }
+    localStorage.setItem("TODO", JSON.stringify(LIST));
+});
+
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------*/
